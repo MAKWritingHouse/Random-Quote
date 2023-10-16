66f7f0fc266f9c052f5f8ec4fe031bd1f4ad3cd6
MAK Writing House <134236495+MAKWritingHouse@users.noreply.github.com>
2023-10-17 00:18:56 +0500

    remove unnecessary code and add commits
    Co-authored-by: MAK Writing House <134236495+MAKWritingHouse@users.noreply.github.com>
    Co-authored-by: Mohammad Moiz Ali <134236495+MAKWritingHouse@users.noreply.github.com>

--- src/app/api/quote/route.ts
+++ src/app/api/quote/route.ts
@@ -1,17 +1,24 @@
+// Import the necessary dependencies
 import { NextResponse } from 'next/server';
-import prisma from '../../../../prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-/**
- * Handles an HTTP GET request to fetch and return a random quote.
- *
- * @returns {NextResponse} A NextResponse object with a JSON response containing a random quote.
- */
+// Create an instance of the Prisma client
+const prisma = new PrismaClient();
+
+// Define an async function called `GET`
 export async function GET() {
+	// Get the total count of quotes in the database
 	const count = await prisma.quote.count();
+
+	// Generate a random index within the range of the quote count
 	const randomIndex = Math.floor(Math.random() * count);
+
+	// Retrieve a single quote from the database using the random index
 	const quote = await prisma.quote.findMany({
 		skip: randomIndex,
 		take: 1,
 	});
+
+	// Return the quote as a JSON response
 	return NextResponse.json(quote[0]);
 }
--- src/app/components/home/index.tsx
+++ src/app/components/home/index.tsx
@@ -4,38 +4,52 @@ import { IQuote } from '../../interfaces/home';
 import { useLayoutEffect, useState } from 'react';
 import './style.scss';
 
+// Function to fetch quotes from the server
 const fetchQuotes = async (): Promise<IQuote> => {
 	try {
+		// Send a GET request to the '/api/quote' endpoint
 		const response = await fetch(`/api/quote`).then((res) => {
+			// Parse the response as JSON
 			return res.json();
 		});
+		// Return the response data
 		return response;
 	} catch (error) {
+		// Log an error message if there is an error fetching quotes
 		console.error('Error fetching quotes:', error);
 	}
 };
 
 const Home = () => {
+	// State variables to store the current quote and the text of the "Copy to Clipboard" button
 	const [quote, setQuote] = useState<IQuote>({
 		quote: '',
 		author: '',
 	});
 	const [copyBtnText, setCopyBtnText] = useState<string>('Copy to Clipboard');
 
+	// Function to copy the current quote to the clipboard
 	const copyToClipboard = (): void => {
+		// Use the Clipboard API to write the quote to the clipboard
 		navigator.clipboard
 			.writeText(`${quote.quote} - ${quote.author}`)
 			.then(() => {
+				// Update the text of the "Copy to Clipboard" button to indicate that the quote has been copied
 				setCopyBtnText('Copied');
+				// Reset the text of the "Copy to Clipboard" button after a delay
 				setTimeout(() => setCopyBtnText('Copy to Clipboard'), 2500);
 			});
 	};
 
+	// Function to handle fetching and updating the quote
 	const handleQuotes = async () => {
+		// Fetch a new quote from the server and update the state variable
 		setQuote(await fetchQuotes());
+		// Reset the text of the "Copy to Clipboard" button
 		setCopyBtnText('Copy to Clipboard');
 	};
 
+	// Use the useLayoutEffect hook to fetch and update the quote when the component is mounted
 	useLayoutEffect(() => {
 		handleQuotes();
 	}, []);
@@ -46,9 +60,11 @@ const Home = () => {
 			<p className="subheading">Get Inspired and Uplifted!</p>
 			<div className="quote-container">
 				<div id="quoteDisplay" className="quote">
+					{/* Display the current quote */}
 					{quote.quote} - {quote.author}
 				</div>
 				<div className="button-container">
+					{/* Button to fetch a new quote */}
 					<button
 						id="generateBtn"
 						className="generate-btn"
@@ -56,9 +72,11 @@ const Home = () => {
 					>
 						Random Quote
 					</button>
+					{/* Button to copy the current quote to the clipboard */}
 					<button id="copyBtn" className="copy-btn" onClick={copyToClipboard}>
 						{copyBtnText}
 					</button>
+					{/* Link to tweet the current quote */}
 					<a
 						id="tweetBtn"
 						className="tweet-btn"
